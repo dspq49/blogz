52b6c51d2b5c03d08e875490863a2dda1a82f505
Danielle Patton <dsashapatton@gmail.com>
2018-04-25 00:21:50 -0500

    almost to step 3

--- main.py
+++ main.py
@@ -18,12 +18,9 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
     
-    
-
     def __init__(self, title, body):
         self.title = title
         self.body = body
-       
 
     def __repr__(self):
         return '<Blog %r>' % self.title % self.body
@@ -33,60 +30,66 @@ class Blog(db.Model):
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
-def index():
+def validate_blog():
+    #client should see form for a new blog post
+    #form should have 2 fields (title/body) and submit button
+    #if fields are blank then client should receive error message
+    #if valid, information should be accepting in database,and client should be redirected to
+    #main page
+    #submit is post request 
+    #if get req client should see form for new post
 
     if request.method == 'POST':
         
-        blog_title = request.form['title']
-        blog_body = request.form['body']
+        title = request.form['title']
+        body = request.form['body']
         
-        nobody_error = ""
         notitle_error = ""
+        nobody_error = ""
+        
 
-        if blog_title == "":
+        if title == "":
             notitle_error = "Please enter a title"
-
         if body == "":
             nobody_error = "Please enter words in body"
-            return render_template('newpost.html', blog_title =blog_title, blog_body=blog_body,
-                notitle_error=notitle_error, nobody_error = nobody_error)
-       
+        
+        if notitle_error or nobody_error:
+         
+            return render_template('newpost.html', title=title, body=body, notitle_error=notitle_error, nobody_error=nobody_error)
        
         else:
-            new_blog = Blog(blog_title, blog_body)      
+            new_blog = Blog(title, body)      
             db.session.add(new_blog)
             db.session.commit()
         
-        
-            return redirect('/blogs?blog_id=' + str(new_blog.blog_id))
-    
-    else: 
-        return render_template('newpost.html', title="", body="", notitle_error="", nobody_error="")
-    
-    
-    if request.method == 'GET':
-        return render_template('blogs.html')
+            return redirect('/blogs') 
 
+    return render_template('newpost.html')
 
+#First, set up the blog so that the "add a new post" form and the blog listings are on the same page, 
+# as with Get It Done!, 
+# and then separate those portions into separate routes, handler classes, and templates. 
+# For the moment, when a user submits a new post, redirect them to the main blog page.
 @app.route('/blogs', methods=['GET'])   
-def mainpage():
-    
-    id = request.args.get('id')
+def all_blogs():
+    #when people go to blogs, they should see all blogs 
+    #GET requests only
+    #id = request.args.get('id')
 
-    if id == '':
-        blogs = Blog.query.all()
-        return render_template('blogs.html', title="Build A Blog", blogs=blogs)
+    #if not id :
+    blogs = Blog.query.all()
+    return render_template('blogs.html', title="Build A Blog", blogs=blogs)
 
-    else:
-        id = str(id)
-        blogs = Blog.query.get(id)
-        return render_template('onepost.html', title="Single Entry", blogs=blogs)
+    #else:
+     #   id = str(id)
+      #  blogs = Blog.query.get(id)
+       #return render_template('onepost.html', title="Single Entry", blogs=blogs)
         
-        #, blog_title=blogs.blog_title, body=blogs.blog_body)
-      
+
 
 @app.route('/')
-def pointtoblogs():
+def index():
+    #index function doesn't show anything, redirects to blogs route 
     return redirect('/blogs')
 
 
